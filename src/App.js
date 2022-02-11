858bda3bec83b7b33e98ca37e0d901cf49371c74
Mohamed.Taher <mohamed.taher@baianat.com>
2022-02-11 23:41:06 +0200

    refactor: upd-style

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ function App() {
   const [newPerson, setNewPerson] = useState("");
   const [langData, setLangData] = useState(formJson);
 
-  useEffect(()=>{
-    (async ()=> {
+  useEffect(() => {
+    (async () => {
       const langObj = await getLang(lang);
       const data = langObj.data;
 
       setTitle(data.title);
       setLangData(
-        langData.map(d=>({
+        langData.map(d => ({
           ...d,
           label: data.fields[d.key]
         }))
@@ -33,15 +33,15 @@ function App() {
   const onLogin = async (e) => {
     const name = e.target.value;
     const ret = await getCheckUserName(name);
-    if(ret.data.error){
+    if (ret.data.error) {
       setLogged(true);
     }
   }
 
-  const onSubmit = async(model) => {
+  const onSubmit = async (model) => {
 
     let newP = await addUser(model);
-    if(newP.error){
+    if (newP.error) {
       alert(newP.error || 'this name actually exist')
       return;
     }
@@ -53,29 +53,34 @@ function App() {
     alert(stringifiedModel);
   }
 
-  return !logged 
-  ? (
-    <div>
-      <input type='text' onChange={onLogin} placeholder="Enter Your Login Name" />
-    </div>
-  )
-  : !title
-  ? <></>
-  : (
-    <div className="App">
-      <div onChange={onChangeValue}>
-        <input checked={lang==='ar'} type="radio" value="ar" name="lang" /> عربى
-        <input checked={lang==='en'} type="radio" value="en" name="lang" /> English
+  return !logged
+    ? (
+      <div className='login-cont'>
+        <input
+          type='text'
+          onChange={onLogin}
+          className='login-inp'
+          placeholder="Enter Your Login Name"
+        />
       </div>
-      <DynamicForm
-        form={langData}
-        onSubmit={onSubmit}
-        title={title}
-        lang={lang}
-      />
-      <div className='new-person'>{newPerson}</div>
-    </div>
-  );
+    )
+    : !title
+      ? <></>
+      : (
+        <div className="App">
+          <div onChange={onChangeValue}>
+            <input checked={lang === 'ar'} type="radio" value="ar" name="lang" /> عربى
+            <input checked={lang === 'en'} type="radio" value="en" name="lang" /> English
+          </div>
+          <DynamicForm
+            form={langData}
+            onSubmit={onSubmit}
+            title={title}
+            lang={lang}
+          />
+          <div className='new-person'>{newPerson}</div>
+        </div>
+      );
 }
 
 export default App;
